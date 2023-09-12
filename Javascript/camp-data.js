82b647317fe43184e04b520df9088de4d7ddeb09
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-09-12 12:43:53 +0100

    Create

---
+++ Javascript/camp-data.js
@@ -0,0 +1,50 @@
+export const camps = [
+    {
+        id: 1,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    },
+    {
+        id: 2,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    },
+    {
+        id: 3,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    },
+    {
+        id: 4,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    },
+    {
+        id: 5,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    },
+    {
+        id: 6,
+        name: "",
+        creator: "",
+        price: "",
+        short_description: "",
+        long_description: ""
+    }
+];
